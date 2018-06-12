8e06f10428b82e83f751a395de38edeaaaec7a78
Andrzej35 <Andrzej35@users.noreply.github.com>
2018-06-13 00:41:17 +0100

    fetching single post by xhr requests on click

--- src/components/PostSingle/index.js
+++ src/components/PostSingle/index.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import classes from './style.css'; 
 
 class PostSingle extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className={classes.PostSingle}>
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className={classes.Edit}>
-                    <button className={classes.Delete}>Delete</button>
-                </div>
-            </div>
 
-        );
+    state = {
+        singlePost: null
+    }
+
+    componentDidUpdate() {
+        if(this.props.id) {
+            if(!this.state.singlePost || (this.state.singlePost && this.state.singlePost.id !== this.props.id)) {
+                axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+                    .then(res => {
+                        this.setState({
+                            singlePost: res.data
+                        })
+                    })
+            }
+        }
+    }
+    render () {
+        let post = <p className={classes.ParagraphCenter}>Please select a Post!</p>
+        if(this.props.id) {
+            post = <p className={classes.ParagraphCenter}>loading...</p>
+        }
+        if(this.state.singlePost) {
+            post = (
+                <div className={classes.PostSingle}>
+                    <h1>{this.state.singlePost.title}</h1>
+                    <p>{this.state.singlePost.body}</p>
+                    <div className={classes.Edit}>
+                        <button className={classes.Delete}>Delete</button>
+                    </div>
+                </div>
+    
+            );
+        }
+        
         return post;
     }
 }
