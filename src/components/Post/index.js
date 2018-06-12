9a945bae8793beca801ac6f948e01005797c1d2d
Andrzej35 <Andrzej35@users.noreply.github.com>
2018-06-13 00:43:19 +0100

    adding to each post click function

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './style.css';
 
 const post = (props) => (
-    <article className={classes.Post}>
+    <article className={classes.Post} onClick={props.clicked}>
         <h3>{props.title}</h3>
         <div className={classes.Info}>
             <div className={classes.Author}>{props.author}</div>
