72b6256328e3e7494c9f9abf1ab558dbe9844436
Andrzej35 <Andrzej35@users.noreply.github.com>
2018-06-12 23:53:32 +0100

    update index.js fetch posts

--- src/containers/Blog/index.js
+++ src/containers/Blog/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Post from '../../components/Post';
 import PostSingle from '../../components/PostSingle';
@@ -6,13 +7,28 @@ import PostNew from '../../components/PostNew';
 import classes from './style.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                const posts = res.data.slice(0, 4)
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Andrzej'
+                    }
+                })
+                this.setState({posts: updatedPosts})
+            })
+    }
     render () {
+        const posts = this.state.posts.map(post => <Post key={post.id} title={post.title} author={post.author} />)
         return (
             <div>
                 <section className={classes.Posts}>
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <PostSingle />
